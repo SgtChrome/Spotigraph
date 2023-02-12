f2f6e40b561818d8abd6590782fe6856fcf0fa85
SgtChrome <42889199+SgtChrome@users.noreply.github.com>
2023-02-13 00:21:12 +0100

    update

--- src/components/RacebarChart.vue
+++ src/components/RacebarChart.vue
@@ -1,28 +1,32 @@
 <script setup>
 import { Diagramm } from "./js/mainSum.js";
-import { computed, onMounted, ref, watch } from "vue";
-
+import { computed, onMounted, onUnmounted, ref, watch } from "vue";
 import {
   PlayIcon,
   ArrowPathIcon,
   ChartBarSquareIcon,
   PlayPauseIcon,
+  ClockIcon,
+  PlayCircleIcon,
 } from "@heroicons/vue/24/solid";
 
 const props = defineProps(["data"]);
 const emit = defineEmits(["externalDate"]);
+const singleSong = ref(false);
+const externalDate = ref(null);
+const frame = ref(0);
 
 const charti = ref(null);
 let chart = null;
 const pickedYear = ref("alle");
 const pickedMonth = ref(0);
-
-const stop = ref(false);
-const frame = ref(0);
 const onlyLastFrame = ref(false);
+const useListenedTime = ref(true);
+
 const stoppedFrameIndex = ref(0);
-const singleSong = ref(false);
-const externalDate = ref(null);
+const running = ref(false);
+const ready = ref(true);
+const restartWish = ref(false);
 
 /*
 let startTime = new Date().getSeconds();
@@ -68,18 +72,42 @@ function filterData(json, year, month) {
   return filteredData;
 }
 
-const replay = () => {
-  if (pickedYear.value == "alle") {
+function replay() {
+  if (running.value) {
+    running.value = false;
+    restartWish.value = true;
+  } else {
+    replayInner();
+  }
+}
+
+function replayInner() {
+  if (pickedYear.value === "alle") {
     chart.initData(props.data);
   } else {
     chart.initData(filterData(props.data, pickedYear.value, pickedMonth.value));
   }
-  stop.value = false;
-  chart.replay();
-};
-function stopReplay() {
-  stop.value = !stop.value;
-  if (!stop.value) {
+  running.value = true;
+  ready.value = false;
+  restartWish.value = false;
+  chart.replay().then(() => {
+    ready.value = true;
+    running.value = false;
+  });
+}
+
+watch(ready, () => {
+  if (ready.value) {
+    if (restartWish.value) {
+      restartWish.value = false;
+      replayInner();
+    }
+  }
+});
+
+function playPause() {
+  running.value = !running.value;
+  if (running.value) {
     chart.replay(stoppedFrameIndex.value);
   }
 }
@@ -111,6 +139,12 @@ function getMonth(month) {
       return "Dezember";
   }
 }
+/* watch(running, () => {
+  console.log("running: " + running.value);
+});
+watch(restartWish, () => {
+  console.log("restartWish: " + restartWish.value);
+}); */
 function clickedYear() {
   pickedMonth.value = false;
   replay();
@@ -124,17 +158,17 @@ watch(onlyLastFrame, () => {
   }
 });
 watch(externalDate, () => {
-  console.log("externalDate changed");
   if (externalDate.value) {
     emit("externalDate", externalDate.value);
   }
 });
-const playPause = computed(() => {
-  return stop.value ? "Play" : "Stop";
+const playPauseText = computed(() => {
+  if (running.value && !onlyLastFrame.value) return "Pause";
+  else return "Play";
 });
 function playPauseClicked() {
   if (!onlyLastFrame.value) {
-    stopReplay();
+    playPause();
   }
 }
 function replayClicked() {
@@ -142,82 +176,140 @@ function replayClicked() {
     replay();
   }
 }
+const ratio = 2.46;
+function resize() {
+  const width = `${
+    (window.innerWidth + 470 - window.innerHeight * ratio) / 2
+  }px`;
+  document.getElementById("leftpadding").style.width = width;
+  document.getElementById("rightpadding").style.width = width;
+}
 onMounted(() => {
+  resize();
+  window.addEventListener("resize", resize);
+
   chart = new Diagramm(
     props.data,
     charti.value,
     frame,
-    stop,
+    running,
     onlyLastFrame,
     stoppedFrameIndex,
     singleSong,
-    externalDate
+    externalDate,
+    useListenedTime
   );
-  chart.replay();
+  replay();
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", resize);
+});
+watch(useListenedTime, () => {
+  console.log(useListenedTime.value);
+  replay();
 });
 </script>
 
 <template>
-  <div
-    class="px-5 py-2 mt-5 bg-gray-500 backdrop-blur-xl rounded-xl bg-opacity-30"
-    id="racebarChart"
-  >
-    <!-- <span class="text-lg"> Frame: {{ frame }} FPS: {{ fps }}</span> -->
+  <div class="flex flex-row">
+    <div id="leftpadding"></div>
+
     <div
-      class="relative justify-center pb-[calc(100vh-292px)] inline-block w-full overflow-hidden text-sm align-top rounded-md h-min"
-      ref="charti"
-    />
-  </div>
-  <div class="flex flex-row py-6">
-    <div class="flex space-x-2">
-      <button
-        class="flex"
-        :class="[onlyLastFrame ? 'defaultButtonDeactivated' : 'defaultButton']"
-        @click="replayClicked"
-      >
-        <ArrowPathIcon class="mr-2 w-7 h-7" />
-        Replay
-      </button>
-      <button
-        class="flex"
-        @click="playPauseClicked"
-        :class="[onlyLastFrame ? 'defaultButtonDeactivated' : 'defaultButton']"
-      >
-        <PlayPauseIcon v-if="!stop" class="mr-1 w-7 h-7" />
-        <PlayIcon v-if="stop" class="mr-2 w-7 h-7" />
-        {{ playPause }}
-      </button>
-      <input
-        type="checkbox"
-        name="onlyLastFrameCheckBox"
-        id="onlyLastFrameValue"
-        class="hidden peer"
-        v-model="onlyLastFrame"
+      class="flex w-full px-5 py-2 mt-5 bg-gray-500 flex-grow-1 backdrop-blur-xl rounded-xl bg-opacity-30"
+      id="racebarChart"
+    >
+      <!-- <span class="text-lg"> Frame: {{ frame }} FPS: {{ fps }}</span> pb-[calc(100vh-292px)]-->
+      <div
+        class="relative w-full inline-block justify-center overflow-hidden text-sm align-top rounded-md pb-[37%]"
+        ref="charti"
       />
-      <label class="flex defaultButton" for="onlyLastFrameValue">
-        <ChartBarSquareIcon
-          class="absolute mr-2 w-7"
-          :class="[onlyLastFrame ? 'animate-ping' : '']"
-        />
-        <ChartBarSquareIcon class="mr-2 w-7" />
-        <span class="flex content-end self-end align-text-bottom"
-          >Endergebnis</span
-        >
-      </label>
     </div>
-    <div class="flex flex-col w-full pt-2 pl-16">
-      <ul class="flex space-x-2">
+    <div id="rightpadding"></div>
+  </div>
+
+  <div class="flex flex-row pt-6">
+    <div class="flex flex-col space-y-2">
+      <div class="flex space-x-2">
+        <button
+          class="flex"
+          :class="[
+            onlyLastFrame ? 'defaultButtonDeactivated' : 'defaultButton',
+          ]"
+          @click="replayClicked"
+        >
+          <ArrowPathIcon class="mr-2 w-7 h-7" />
+          Replay
+        </button>
+        <button
+          class="flex"
+          @click="playPauseClicked"
+          :class="[
+            onlyLastFrame ? 'defaultButtonDeactivated' : 'defaultButton',
+          ]"
+        >
+          <PlayPauseIcon
+            v-if="running && !onlyLastFrame"
+            class="mr-1 w-7 h-7"
+          />
+          <PlayIcon v-if="!running || onlyLastFrame" class="mr-2 w-7 h-7" />
+          {{ playPauseText }}
+        </button>
+        <input
+          type="checkbox"
+          name="onlyLastFrameCheckBox"
+          id="onlyLastFrameValue"
+          class="hidden peer"
+          v-model="onlyLastFrame"
+        />
+        <label class="flex defaultButton" for="onlyLastFrameValue">
+          <ChartBarSquareIcon
+            class="absolute mr-2 w-7"
+            :class="[onlyLastFrame ? 'animate-ping' : '']"
+          />
+          <ChartBarSquareIcon class="mr-2 w-7" />
+          <span class="flex content-end self-end align-text-bottom"
+            >Endergebnis</span
+          >
+        </label>
+      </div>
+      <div class="w-fit">
+        <input
+          type="checkbox"
+          name="useListenedTimeCheckBox"
+          id="useListenedTimeValue"
+          class="hidden peer"
+          v-model="useListenedTime"
+        />
+        <label class="flex defaultButtonAlwaysOn" for="useListenedTimeValue">
+          <ClockIcon
+            v-if="useListenedTime"
+            class="absolute mr-2 w-7 animate-ping"
+          />
+          <ClockIcon v-if="useListenedTime" class="mr-2 w-7" />
+          <PlayCircleIcon
+            v-if="!useListenedTime"
+            class="absolute mr-2 w-7 animate-ping"
+          />
+          <PlayCircleIcon v-if="!useListenedTime" class="mr-2 w-7" />
+          <span class="flex content-end self-end align-text-bottom">{{
+            useListenedTime ? "Zeitdauer" : "Anzahl Hits"
+          }}</span>
+        </label>
+      </div>
+    </div>
+    <div class="flex flex-col w-full h-full pl-16">
+      <ul class="flex pt-1 space-x-2 h-fit">
         <li>
           <input
             class="hidden peer"
             type="radio"
-            name="months"
+            name="years"
             id="alle"
             value="alle"
             v-model="pickedYear"
-            @click="clickedYear"
+            @change="clickedYear"
           />
-          <label class="defaultButton" for="alle">Alle</label>
+          <label class="defaultDatetimeButton" for="alle">Alle</label>
         </li>
         <li v-for="year in yearsArray" :key="year">
           <input
@@ -227,9 +319,9 @@ onMounted(() => {
             :id="year"
             :value="year"
             v-model="pickedYear"
-            @click="clickedYear"
+            @change="clickedYear"
           />
-          <label class="defaultButton" :for="year">{{ year }}</label>
+          <label class="defaultDatetimeButton" :for="year">{{ year }}</label>
         </li>
       </ul>
       <ul class="flex mt-5 space-x-2">
@@ -241,9 +333,11 @@ onMounted(() => {
             :id="month"
             :value="month"
             v-model="pickedMonth"
-            @click="clickedMonth"
+            @change="clickedMonth"
           />
-          <label class="defaultButton" :for="month"> {{ getMonth(month) }}</label>
+          <label class="defaultDatetimeButton" :for="month">
+            {{ getMonth(month) }}</label
+          >
         </li>
       </ul>
     </div>
@@ -252,10 +346,16 @@ onMounted(() => {
 
 <style>
 .defaultButton {
-  @apply px-4 py-2 text-xl font-semibold text-white bg-white peer-checked:border-white rounded-md cursor-pointer peer-checked:border-2 bg-opacity-30 hover:outline-2 hover:shadow-2xl focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:bg-slate-600 hover:bg-opacity-30 border-transparent border-2 select-none h-12;
+  @apply px-3 py-1 text-xl font-semibold text-white bg-white peer-checked:border-white rounded-md cursor-pointer peer-checked:border-2 bg-opacity-30 hover:outline-2 hover:shadow-2xl focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:bg-slate-600 hover:bg-opacity-30 border-transparent border-2 select-none h-10;
+}
+.defaultButtonAlwaysOn {
+  @apply px-3 py-1 text-xl font-semibold text-white bg-white border-white rounded-md cursor-pointer bg-opacity-30 hover:outline-2 hover:shadow-2xl focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:bg-slate-600 hover:bg-opacity-30 border-2 select-none h-10;
 }
 .defaultButtonDeactivated {
-  @apply px-4 py-2 text-xl cursor-not-allowed font-semibold text-gray-400 bg-gray-700 rounded-md peer-checked:border-2 bg-opacity-30 hover:outline-2 hover:shadow-2xl focus:outline-none border-transparent border-2 select-none h-12;
+  @apply px-3 py-1 text-xl cursor-not-allowed font-semibold text-gray-400 bg-gray-800 rounded-md peer-checked:border-2 bg-opacity-30 hover:outline-2 hover:shadow-2xl focus:outline-none border-transparent border-2 select-none h-10;
+}
+.defaultDatetimeButton {
+  @apply px-4 py-[6px] my-2 text-xl font-semibold text-white bg-white peer-checked:border-white rounded-md cursor-pointer peer-checked:border-2 bg-opacity-30 hover:outline-2 hover:shadow-2xl focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:bg-slate-600 hover:bg-opacity-30 border-transparent border-2 select-none h-10;
 }
 .svg-content-responsive {
   display: inline-block;
