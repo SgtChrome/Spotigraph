720e2b35a3457b5b857faa70a29dc8a60c902f51
SgtChrome <42889199+SgtChrome@users.noreply.github.com>
2023-02-28 01:12:46 +0100

    change responsiveness

--- src/components/SpotifyPreview.vue
+++ src/components/SpotifyPreview.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="hidden 2xl:flex w-[30%]" :class="[spotifyUri ? 'pt-2' : '']">
     <div class="flex pt-2 backdrop-blur-xl" id="embed-iframe" />
-    <div v-if="!spotifyUri" class="flex justify-center items-center px-2 py-px mx-2 my-1 text-white transition-all duration-500 ease-in-out border-2 border-transparent cursor-pointer lg:py-3 lg:m-4 lg:px-4 w-fit text-ellipsis rounded-xl backdrop-blur-xl bg-opacity-40 hover:outline-2 hover:shadow-2xl focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:bg-slate-600 hover:bg-opacity-30 bg-gray-300 text-sm lg:text-2xl">
-      <span class="">Click on a song to preview it on Spotify</span>
+    <div
+      v-if="!spotifyUri"
+      class="flex items-center justify-center py-px my-1 text-sm text-white transition-all duration-500 ease-in-out bg-gray-300 border-2 border-transparent cursor-pointer lg:py-3 lg:my-4 lg:px-4 w-fit text-ellipsis rounded-xl backdrop-blur-xl bg-opacity-40 hover:outline-2 hover:shadow-2xl focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:bg-slate-600 hover:bg-opacity-30 lg:text-2xl"
+    >
+      <span class="whitespace-nowrap">Click on a song to preview it on Spotify</span>
     </div>
     <div class="flex items-center pb-2 pl-3 w-[64px]">
-      <span v-show="loading" class="loader p-1"></span>
+      <span v-show="loading" class="p-1 loader"></span>
     </div>
   </div>
 </template>
@@ -24,22 +27,28 @@ const props = defineProps({
 });
 
 function fetchSpotifyUri(newVal) {
-  const devurl = "http://127.0.0.1:8000"
-  const produrl = "https://spotigraph-backend.ey.r.appspot.com"
-  fetch(produrl + "/searchSpotify?track=" + newVal[0].replace(' ', '%20') + "&artist=" + newVal[1].replace(' ', '%20'),
-      {
-    "method": "GET",
-    "mode": "cors",
-    "headers": {
-      "Content-Type": "application/json",
-    },
-  })
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    if (data) EmbedController.loadUri(data);
-    else loading.value = false;
-  });
+  const devurl = "http://127.0.0.1:8000";
+  const produrl = "https://spotigraph-backend.ey.r.appspot.com";
+  fetch(
+    produrl +
+      "/searchSpotify?track=" +
+      newVal[0].replace(" ", "%20") +
+      "&artist=" +
+      newVal[1].replace(" ", "%20"),
+    {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      if (data) EmbedController.loadUri(data);
+      else loading.value = false;
+    });
 }
 
 watch(
@@ -59,7 +68,7 @@ watch(
 
       window.SpotifyIFrameAPI.createController(element, options, callback);
 
-      EmbedController.addListener('ready', () => {
+      EmbedController.addListener("ready", () => {
         loading.value = false;
       });
     }
@@ -87,7 +96,7 @@ onMounted(() => {
 .loader {
   width: 48px;
   height: 48px;
-  border: 5px solid #FFF;
+  border: 5px solid #fff;
   border-bottom-color: transparent;
   border-radius: 50%;
   display: inline-block;
